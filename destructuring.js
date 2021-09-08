91e6365b69e1982f0863e43972c1275e02af4a0d
vtsang <vtsang@luk-pf0uh3yr.corp.capgemini.com>
2021-09-08 19:09:17 +0100

    Leaarning Destructuring Assignment from MDN Docs.

--- destructuring.js
+++ destructuring.js
@@ -201,3 +201,71 @@ foo( {
     b: 2,
     c: 3
 });
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+Destructuring assignment:
+
+The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects,
+into distinct variables.
+*/
+
+let a, b, rest;
+[a, b] = [10, 20];
+
+console.log(a);
+// expected output: 10
+
+console.log(b);
+// expected output: 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+
+console.log(rest);
+// expected output: Array [30,40,50]
+
+// Syntax:
+let a, b, rest;
+[a, b] = [10, 20];
+console.log(a); // 10
+console.log(b); // 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // [30, 40, 50]
+
+({ a, b } = { a: 10, b: 20 });
+console.log(a); // 10
+console.log(b); // 20
+
+// Stage 4(finished) proposal
+({a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40});
+console.log(a); // 10
+console.log(b); // 20
+console.log(rest); // {c: 30, d: 40}
+
+// Description:
+// The object and array literal expressions provide an easy way to create ad hoc packages of data.
+const x = [1, 2, 3, 4, 5];
+
+// The destructuring assignment uses similar syntax, but on the left-hand side of the assignment to define what values to unpack from the sourced variable.
+const x = [1, 2, 3, 4, 5];
+const [y, z] = x;
+console.log(y); // 1
+console.log(z); // 2
+
+// Similarly, you can destructure arrays on the left-hand side of the assignment
+const [firstElement, secondElement] = list;
+// is equivalent to:
+// const firstElement = list[0];
+// const secondElement = list[1];
+
+
+
+
+
+
+
+
