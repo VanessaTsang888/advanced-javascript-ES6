0dcb6466a4c3b571be1ba7d04bea4476e3989fcf
vtsang <vtsang@luk-5cg01162zg.corp.capgemini.com>
2021-09-09 12:01:38 +0100

    Completed the MDN Docs: Array Destructuring

--- destructuring.js
+++ destructuring.js
@@ -262,6 +262,118 @@ const [firstElement, secondElement] = list;
 // const firstElement = list[0];
 // const secondElement = list[1];
 
+/*
+
+Examples: Array destructuring
+Basic variable assignment:
+
+*/
+const foo = ['one', 'two', 'three'];
+
+const [red, yellow, green] = foo;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // "three"
+
+// Assignment separate from declaration
+// A variable can be assigned its value via destructuring, separate from the variable's declaration.
+let a, b;
+
+[a, b] = [1, 2];
+console.log(a); // 1
+console.log(b); // 2
+
+// In an array destructuring from an array of length N specified on the right-hand side of the assignment, if the number of variables
+// specified on the left-hand side of the assignment is greater than N, only the first N variables are assigned values. The values of the
+// remaining variables will be undefined.
+const foo = ['one', 'two'];
+
+const [red, yellow, green, blue] = foo;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // undefined
+console.log(blue);  //undefined
+
+/// Default values:
+// A variable can be assigned a default, in the case that the value unpacked from the array is undefined.
+let a, b;
+
+[a=5, b=7] = [1];
+console.log(a); // 1
+console.log(b); // 7
+
+/// Swapping variables
+// Two variables values can be swapped in one destructuring expression.
+// Without destructuring assignment, swapping two values requires a temporary variable.
+let a = 1;
+let b = 3;
+
+[a, b] = [b, a];
+console.log(a); // 3
+console.log(b); // 1
+
+const arr = [1,2,3];
+[arr[2], arr[1]] = [arr[1], arr[2]];
+console.log(arr); // [1,3,2]
+// I don't understand how the managed to get between the 1 and 2? I will ask my mentor this afternoon.
+
+/*
+Parsing an array returned from a function
+It's always been possible to return an array from a function. Destructuring can make working with an array return value more concise.
+
+In this example, f() returns the values [1, 2] as its output, which can be parsed in a single line with destructuring.
+*/
+
+function f() {
+  return [1, 2];
+}
+
+let a, b;
+[a, b] = f();
+console.log(a); // 1
+console.log(b); // 2
+
+/// Ignoring some returned values.
+// You can ignore return values that you're not interested in:
+function f() {
+  return [1, 2, 3];
+}
+
+const [a, , b] = f();
+console.log(a); // 1
+console.log(b); // 3
+
+const [c] = f();
+console.log(c); // 1
+
+// You can also ignore all returned values:
+[,,] = f();
+
+/// Assigning the rest of an array to a variable:
+// When destructuring an array, you can unpack and assign the remaining part of it to a variable using the rest pattern:
+const [a, ...b] = [1, 2, 3];
+console.log(a); // 1
+console.log(b); // [2, 3]
+
+///  Unpacking values from a regular expression match
+// When the regular expression exec() method finds a match, it returns an array containing first the entire matched portion
+// of the string and then the portions of the string that matched each parenthesized group in the regular expression.
+// Destructuring assignment allows you to unpack the parts out of this array easily, ignoring the full match if it is not needed.
+function parseProtocol(url) {
+  const parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url);
+  if (!parsedURL) {
+    return false;
+  }
+  console.log(parsedURL);
+  // ["https://developer.mozilla.org/en-US/docs/Web/JavaScript", 
+  // "https", "developer.mozilla.org", "en-US/docs/Web/JavaScript"]
+
+  const [, protocol, fullhost, fullpath] = parsedURL;
+  return protocol;
+}
+
+console.log(parseProtocol('https://developer.mozilla.org/en-US/docs/Web/JavaScript'));
+// "https"
 
 
 
